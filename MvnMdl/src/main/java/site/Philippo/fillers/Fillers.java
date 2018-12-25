3e0e09f92ed22c5aa3da8d290556977631f01b0c
philip <lPhilip04@gmail.com>
2018-12-25 18:12:29 +0200

    Added function to check input parameters

--- MvnMdl/src/main/java/site/Philippo/fillers/Fillers.java
+++ MvnMdl/src/main/java/site/Philippo/fillers/Fillers.java
@@ -1,6 +1,5 @@
 package site.Philippo.fillers;
 
-import java.util.Arrays;
 
 /**
  * Static methods to fill arrays of integer
@@ -22,6 +21,7 @@ public class Fillers {
 	 */
 	@ArrayFiller(name = "Ascending filler")
 	public static void initArrayWithAscendingOrder(int[] arr, int start, int end) {
+		paramCheck(arr, start, end);
 		for (int i = start; i < end; i++) {
 			arr[i] = i;
 		}
@@ -40,6 +40,7 @@ public class Fillers {
 	 */
 	@ArrayFiller(name = "Descending filler")
 	public static void initArrayWithDescendingOrder(int[] arr, int start, int end) {
+		paramCheck(arr, start, end);
 		end -= 1;
 		int contetEnd = end;
 		for (int i = start; i <= end; i++) {
@@ -61,6 +62,7 @@ public class Fillers {
 	 */
 	@ArrayFiller(name = "Ascending filler with last random")
 	public static void initArrAscLastRandom(int[] arr, int start, int end) {
+		paramCheck(arr, start, end);
 		for (int i = start; i < end+1; i++) {
 			arr[i] = i;
 		}
@@ -80,9 +82,27 @@ public class Fillers {
 	 */
 	@ArrayFiller(name = "Random filler")
 	public static void initArrRandomFiller(int[] arr, int start, int end) {
+		paramCheck(arr, start, end);
 		for(int i = start; i < end+1; i++){
 			arr[i] = start + (int)(Math.random() * end);
 		}
+	}
+
+	/**
+	 * <pre>
+	 * Used to check input parameters <br/>
+	 * </pre>
+	 *
+	 * @param arr An array to fill
+	 * @param start First wanted element index
+	 * @param end Last wanted element index
+	 * @throws IllegalArgumentException Check the parameters you use
+	 */
+	private static void paramCheck(int[] arr, int start, int end){
+		if (arr == null || arr.length == 0 || start == end ||
+				end > start || end > arr.length - 1 ||
+				start > arr.length - 1 || start < 0 || end < 0)
+			throw new IllegalArgumentException("Parameters correctness test failed");
 	}
 }
 
